db4e8c3ae7e3aa0d5cf1ce0a13b1644b236ba8f4
Laxxup <surezluis4@gmail.com>
2026-02-28 17:55:21 -0600

    V3: Fix debootstrap script para Daedalus

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# 1. Instalar herramientas de empaquetado
+# 1. Instalar dependencias y llaves de Devuan
 sudo apt-get update
-sudo apt-get install -y debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
+sudo apt-get install -y debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools devuan-keyring
 
-# 2. Crear un sistema base minimo (Esto es lo que faltaba)
-sudo debootstrap --variant=minbase --include=linux-image-amd64,lxde-core,network-manager,sudo,fastfetch daedalus ./chroot http://deb.devuan.org/merged
+# 2. Construir el sistema (Usando el script de 'sid' como puente)
+sudo debootstrap --no-check-gpg --variant=minbase --include=linux-image-amd64,lxde-core,network-manager,sudo,fastfetch daedalus ./chroot http://deb.devuan.org/merged /usr/share/debootstrap/scripts/sid
 
-# 3. Inyectar tu wallpaper
+# 3. Inyectar wallpaper
 sudo mkdir -p ./chroot/usr/share/images/desktop-base/
 sudo cp wallpaperITCMOS.jpg ./chroot/usr/share/images/desktop-base/
 
-# 4. Crear la estructura de la ISO
+# 4. Empaquetar
 mkdir -p image/live
 sudo mksquashfs chroot image/live/filesystem.squashfs -comp xz -e boot
 
-# 5. Copiar archivos de arranque
-sudo cp chroot/boot/vmlinuz* image/live/vmlinuz
-sudo cp chroot/boot/initrd* image/live/initrd
+# 5. Copiar Kernel (Asegurando la ruta)
+sudo cp ./chroot/boot/vmlinuz* image/live/vmlinuz
+sudo cp ./chroot/boot/initrd* image/live/initrd
 
-# 6. Generar la ISO final
+# 6. Crear ISO
 grub-mkrescue -o ITCM_OS_v1.iso image
