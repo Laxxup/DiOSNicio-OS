3242fad8cec37a2c4d788bbae18a46c1e12b6fe4
Laxxup <surezluis4@gmail.com>
2026-02-28 18:18:03 -0600

    V10: Instalación en dos pasos y montaje de sistemas virtuales

--- build.sh
+++ build.sh
@@ -4,20 +4,32 @@ echo "=== Instalando herramientas ==="
 sudo apt-get update -qq
 sudo apt-get install -y -qq debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools wget ca-certificates
 
-echo "=== Construyendo base Devuan Daedalus ==="
-# Cambiamos systemd por sysvinit-core y fastfetch por neofetch
-sudo debootstrap --no-check-gpg --variant=minbase --include=linux-image-amd64,sysvinit-core,lxde-core,lightdm,network-manager,sudo,neofetch,locales,tzdata,console-setup daedalus ./chroot http://deb.devuan.org/merged /usr/share/debootstrap/scripts/sid
+echo "=== PASO 1: Construyendo base mínima Devuan Daedalus ==="
+sudo debootstrap --no-check-gpg --variant=minbase --include=linux-image-amd64,sysvinit-core,sudo,locales,tzdata daedalus ./chroot http://deb.devuan.org/merged /usr/share/debootstrap/scripts/sid
+
+echo "=== Configurando entornos para evitar errores de dbus ==="
+sudo chroot ./chroot mount -t proc none /proc
+sudo chroot ./chroot mount -t sysfs none /sys
+sudo chroot ./chroot mount -t devtmpfs none /dev
+
+echo "=== PASO 2: Instalando LXDE y herramientas dentro del Chroot ==="
+sudo chroot ./chroot apt-get update
+sudo chroot ./chroot apt-get install -y --no-install-recommends lxde-core lightdm network-manager neofetch console-setup
 
 echo "=== Configurando locales y timezone ==="
 echo "es_MX.UTF-8 UTF-8" | sudo tee ./chroot/etc/locale.gen
 sudo chroot ./chroot locale-gen
 echo "LANG=es_MX.UTF-8" | sudo tee ./chroot/etc/locale.conf
 echo "America/Monterrey" | sudo tee ./chroot/etc/timezone
-sudo chroot ./chroot dpkg-reconfigure -f noninteractive tzdata
+DEBIAN_FRONTEND=noninteractive sudo chroot ./chroot dpkg-reconfigure tzdata
 
 echo "=== Inyectando wallpaper ==="
 sudo mkdir -p ./chroot/usr/share/backgrounds/
 sudo cp wallpaperITCMOS.jpg ./chroot/usr/share/backgrounds/itcm-wallpaper.jpg
+
+echo "=== Limpiando chroot ==="
+sudo chroot ./chroot umount /proc /sys /dev
+sudo chroot ./chroot apt-get clean
 
 echo "=== Empaquetando squashfs ==="
 mkdir -p image/live
